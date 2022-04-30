85f3a0dfbb058d9018f300fc22fe5e95496df641
Jeff Bruemmer <jeff.bruemmer@gmail.com>
2022-04-29 21:28:46 -0400

    tools

---
+++ tools/build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+## Exit if any command fails.
+set -e
+
+## Set the table.
+
+echo "Initializing build..."
+
+## Remove existing classes directory and clerk binary.
+
+echo "Removing classes directory."
+
+rm -rf classes
+
+echo "Removing .cpcache directory."
+
+rm -rf .cpcache
+
+echo "Building native image."
+
+echo "This could take a bit..."
+
+echo "If you run into issues, check out:"
+
+echo "https://github.com/clj-easy/graal-docs"
+
+clojure -M:build
+
+echo "Native image built successfully."
+
+echo "Making the clerk image executable."
+
+chmod +x clerk
+
+echo "Moving clerk binary to /usr/local/bin directory."
+
+mv clerk /usr/local/bin
+
+## Bus the table
+
+echo "Cleaning up..."
+
+rm -rf classes
+
+rm -rf .cpcache
+
+echo "Done."
+
+echo "Running clerk -h"
+
+clerk -h
+
+
